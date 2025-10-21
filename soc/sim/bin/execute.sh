2c50ec81e48ed1bdc2cada9ef643a9e2830f4936
Anindya Ray <anindya_ray@hotmail.com>
2025-10-21 14:23:41 -0700

    Update execute.sh

--- soc/sim/bin/execute.sh
+++ soc/sim/bin/execute.sh
@@ -1,3 +1,3 @@
-pushd .; cd ../../../tools/or1k-linux/test; ./compile.sh ; popd;
+pushd .; cd ../../test; ./compile.sh ; popd;
 
-cp ../../../tools/or1k-linux/test/tt flash.in; make sim
+cp ../../test/tt flash.in; make sim
