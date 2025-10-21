4da7984f52431ef05ef0f9f45c1b92e55e94b187
quickflow <anindya_ray@hotmail.com>
2025-10-21 13:07:12 -0700

    Delete tools/or1k-linux directory

--- tools/or1k-linux/test/compile.sh
+++
@@ -1,6 +0,0 @@
-../bin/or1k-linux-gcc -nostdlib -nostartfiles -nodefaultlibs -Wl,-Tlinker.ld -Igfx_inc -o test.o or1k_test.c gfx_demo.c
-
-../bin/or1k-linux-objdump -D -x test.o > test.txt
-
-echo "@100" > tt
-od -t x1 -w1 -v --skip-bytes=272 --address-radix=n test.o >> tt
