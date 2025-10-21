c78ca836f6612476d89d0db09b8a4e11f3e8456c
Anindya Ray <anindya_ray@hotmail.com>
2025-10-21 14:17:36 -0700

    Update compile.sh

--- soc/test/compile.sh
+++ soc/test/compile.sh
@@ -1,3 +1,8 @@
-../../tools/or1k-linux/bin/or1k-linux-gcc -nostdlib -nostartfiles -nodefaultlibs -Wl,-Tlinker.ld -o test.o or1k_test.c
+../tools/or1k-linux/bin/or1k-linux-gcc -nostdlib -nostartfiles -nodefaultlibs -Wl,-Tlinker.ld -Igfx_inc -o test.o or1k_tes
+t.c gfx_demo.c
 
-../../tools/or1k-linux/bin/or1k-linux-objdump -D -x test.o > test.txt
+../tools/or1k-linux/bin/or1k-linux-objdump -D -x test.o > test.txt
+
+echo "@100" > tt
+od -t x1 -w1 -v --skip-bytes=272 --address-radix=n test.o >> tt
+
