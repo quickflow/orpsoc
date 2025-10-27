52c8371ba63dee14262bb987598de235443eded1
Anindya Ray <anindya_ray@hotmail.com>
2025-10-27 14:36:31 -0700

    Update toolchain_install.sh

--- soc/toolchain_install.sh
+++ soc/toolchain_install.sh
@@ -2,3 +2,5 @@ mkdir toolchain; cd toolchain
 wget https://github.com/stffrdhrn/or1k-toolchain-build/releases/download/or1k-15.1.0-20250621/or1k-linux-15.1.0-20250621.tar.xz
 xz -d or1k-linux-15.1.0-20250621.tar.xz
 tar -xvf or1k-linux-15.1.0-20250621.tar
+cd ..
+
