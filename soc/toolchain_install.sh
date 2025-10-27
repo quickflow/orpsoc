35a5edca09a41c0d83809a56203a6867f79c604d
Anindya Ray <anindya_ray@hotmail.com>
2025-10-27 14:29:01 -0700

    Add files via upload

---
+++ soc/toolchain_install.sh
@@ -0,0 +1,4 @@
+mkdir toolchain; cd toolchain
+wget https://github.com/stffrdhrn/or1k-toolchain-build/releases/download/or1k-15.1.0-20250621/or1k-linux-15.1.0-20250621.tar.xz
+xz -d or1k-linux-15.1.0-20250621.tar.xz
+tar -xvf or1k-linux-15.1.0-20250621.tar
