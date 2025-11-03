159711aa632d596178e4522f222c3093c944ee4a
Anindya Ray <anindya_ray@hotmail.com>
2025-11-02 19:25:08 -0800

    Add files via upload

--- soc/bench/ATMEL_FLASH/flash_verilog/simple_model/flash_model.v
+++ soc/bench/ATMEL_FLASH/flash_verilog/simple_model/flash_model.v
@@ -8,7 +8,7 @@ module AT26DFxxx
    );
    
    
-   wire	  clk = SCK;
+   wire	  sclk = SCK;
    // SPI Clock (SCK)
    wire	  cs_n = CSB;
    // Chip Select (Active Low)
@@ -17,87 +17,107 @@ module AT26DFxxx
    
    reg	  miso;
    
-   assign      SO = miso;
-   // Master In Slave Out
-   
-   // Parameters
-   parameter MEM_SIZE = 256;
-   // 256 bytes for demo
-   parameter MEMORY_FILE = "memory.txt";
-   // Memory pre-load
-   
-   // Internal memory
-   reg [7:0]	   memory [0:MEM_SIZE-1];
-   
-   // SPI state machine
-   parameter	   IDLE = 0;
-   parameter	   CMD = 1;
-   parameter	   ADDR = 2;
-   parameter	   READ = 3;
-   
-   reg [1:0]	   state;
-   
-   // SPI shift register
-   reg [7:0]	   shift_reg;
-   reg [2:0]	   bit_cnt;
-   reg [23:0]	   addr;
-   reg [7:0]	   data_out;
-   reg [7:0]	   data_latch;
-   
-   // Load memory from file
+   assign      SO = cs_n ? 1'bz : miso;
+
+    // Flash memory array
+    reg [7:0] mem [0:65535];  // 64KB flash
+    reg [23:0] addr;
+    reg [7:0]  cmd;
+    reg [31:0]  shift_in;
+    reg [7:0]  shift_out;
+    reg [7:0]  bit_cnt;
+    reg        write_enable;
+    reg        reading;
+    reg        writing;
+    reg [15:0] wr_ptr;
+
+    // SPI state machine
+   parameter   IDLE = 3'b000;
+   parameter   CMD = 3'b001;
+   parameter   ADDR = 3'b010;
+   parameter   READ = 3'b011;
+   parameter   WRITE = 3'b100;
+
+   reg [2:0]   state;
+
+   parameter   MEMORY_FILE = "memory.txt";	// Memory pre-load
+
    initial begin
-      $readmemh(MEMORY_FILE, memory);
+      $readmemh (MEMORY_FILE, mem);
    end
    
-   always @(negedge clk or posedge cs_n) begin
-      if (cs_n) begin
-	 state <= IDLE;
-	 bit_cnt <= 3'd0;
-      end else begin
-	 shift_reg <= {shift_reg[6:0], mosi};
-	 bit_cnt <= bit_cnt + 1;
-	 
-	 case (state)
-	   IDLE: begin
-	      state <= CMD;
-	      bit_cnt <= 3'd0;
-	   end
-	   
-	   CMD: begin
-	      if (bit_cnt == 3'd7) begin
-		 if (shift_reg[6:0] == 7'h03) begin // Read command
-		    state <= ADDR;
-		 end else begin
-		    state <= IDLE;
-		    // Unsupported command
-		 end
-		 bit_cnt <= 3'd0;
-	      end // if (bit_cnt == 3'd7)
-	   end // case: CMD
-	   
-	   ADDR: begin
-	      if (bit_cnt == 3'd7) begin
-		 addr <= {addr[15:0], shift_reg};
-		 if (&addr[23:16]) begin
-		    state <= READ;
-		    data_out <= memory[addr];
-		    data_latch <= memory[addr];
-		 end
-		 bit_cnt <= 3'd0;
-	      end // if (bit_cnt == 3'd7)
-	   end // case: ADDR
-	   
-	   READ: begin
-	      miso <= data_out[7];
-	      data_out <= {data_out[6:0], 1'b0};
-	      if (bit_cnt == 3'd7) begin
-		 addr <= addr + 1;
-		 data_out <= memory[addr + 1];
-		 data_latch <= memory[addr + 1];
-		 bit_cnt <= 3'd0;
-	      end
-	   end // case: READ
-	 endcase // case (state)
-      end // else: !if(cs_n)
-   end // always @ (negedge clk or posedge cs_n)
-endmodule // spi_flash_model
+
+    always @(negedge cs_n) begin
+        state <= CMD;
+        bit_cnt <= 0;
+        write_enable <= 0;
+        reading <= 0;
+        writing <= 0;
+    end
+
+    always @(posedge sclk) begin
+        if (!cs_n) begin
+            shift_in <= {shift_in[6:0], mosi};
+            bit_cnt <= bit_cnt + 1;
+
+            case (state)
+                CMD: if (bit_cnt == 7) begin
+                    cmd <= {shift_in[6:0], mosi};
+                    bit_cnt <= 0;
+                    state <= ADDR;
+		   $display("[%m] new CMD(%h) received", {shift_in[6:0], mosi});
+                end
+
+                ADDR: if (bit_cnt == 23) begin
+                    addr <= {shift_in[22:0], mosi};
+                    bit_cnt <= 0;
+                    case (cmd)
+                        8'h03: begin  // READ
+//                            shift_out <= mem[addr];
+                            shift_out <= mem[{shift_in[22:0], mosi}];
+                            reading <= 1;
+                            state <= READ;
+			   $display("[%m] Opcode(%h) for Read Array received",cmd);
+                        end
+                        8'h02: begin  // WRITE
+                            if (write_enable) begin
+                                writing <= 1;
+                                state <= WRITE;
+				$display("[%m] Opcode(%h) Byte Program received",cmd);
+                            end else begin
+                                state <= IDLE;
+                            end
+                        end
+                        8'h06: begin  // WREN
+                            write_enable <= 1;
+                            state <= IDLE;
+			   $display("[%m]Opcode(%h) for Write Enable received",cmd);
+                        end
+                        default: state <= IDLE;
+                    endcase
+                end
+
+                READ: begin
+                    miso <= shift_out[7];
+                    shift_out <= {shift_out[6:0], 1'b0};
+                    bit_cnt <= bit_cnt + 1;
+                    if (bit_cnt == 7) begin
+                        addr <= addr + 1;
+                        shift_out <= mem[addr + 1];
+                        bit_cnt <= 0;
+                    end
+                end
+
+                WRITE: begin
+                    if (bit_cnt == 7) begin
+                        mem[addr] <= {shift_in[6:0], mosi};
+                        addr <= addr + 1;
+                        bit_cnt <= 0;
+                    end
+                end
+            endcase
+        end
+    end
+
+endmodule
+
