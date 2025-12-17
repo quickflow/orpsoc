6bc0e7fad5fb593c0412630a1f54e6c0f484b98b
Anindya Ray <anindya_ray@hotmail.com>
2025-12-17 10:11:00 -0800

    Add files via upload

--- soc/sw/sdram_test/main.c
+++ soc/sw/sdram_test/main.c
@@ -394,8 +394,10 @@ void gemm_check_results (int testNum, int base_c, int dw16,
     }
     if (mism==0) {
       passCount++;
+      print("\t\tGemm Test PASS\r\n");
     } else {
       failCount++;
+      print("\t\tGemm Test FAIL\r\n");
     }
   }
 }
@@ -573,6 +575,22 @@ void gemm_test()
 /******************************************************************************/
   GPIO_Write(0x5555dddd);
 /******************************************************************************/
+}
+
+void simple_d2d_test()
+{
+  uint32 count;
+  for(count=0; count<256; count+=4) {
+    REG32(0x1100 + count) = count;
+  }
+
+  REG32(D2D_BASE + D2D_TX_SRC) = 0x11000;
+  REG32(D2D_BASE + D2D_TX_LEN) = 0x100;
+  REG32(D2D_BASE + D2D_TX_CSR) = (1 << D2D_CSR_START);
+
+  GPIO_Write(0xa2000000 + REG32(GEMM_BASE + GEMM_BASE_A));
+  GPIO_Write(0xa2000000 + REG32(GEMM_BASE + GEMM_BASE_B));
+  GPIO_Write(0xa2000000 + REG32(GEMM_BASE + GEMM_BASE_C));
 }
 
 /*$$EXTERNAL EXEPTIONS*/
